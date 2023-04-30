4fe291afdf6e6f5e3d1aa66f8a42caa45b85d2b3
Fruity <fruityflunt@gmail.com>
2023-04-30 08:35:14 +0300

    header & footer: tablet, mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello!");
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".header__info");
+
+hamburger.addEventListener("click", (e) => {
+  hamburger.classList.toggle("is-active");
+  console.log(e.target);
+  if (hamburger.classList.contains("is-active")) {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+});
+
+menu.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.currentTarget === menu) {
+    menu.style.display = "none";
+    hamburger.classList.remove("is-active");
+  }
+});
